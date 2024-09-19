6acca4a8f61e73f32d8059a352f15a0572235471
manojbhandi <bamnojkumar5@gmail.com>
2024-09-19 20:26:09 +0530

    Initialize project using Create React App

---
+++ src/Components/Hero.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+const FeatureHome = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+export default FeatureHome
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+const FeatureHome = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+export default FeatureHome
